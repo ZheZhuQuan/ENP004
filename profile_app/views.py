7e1147386b9012757f5165b68004b0c07d340855
quan <quan_zhezhu@aliber.co.jp>
2020-10-12 11:54:05 +0900

    更新しました

--- profile_app/views.py
+++ profile_app/views.py
@@ -50,3 +50,34 @@ class EmployeeView(generic.ListView, LoginRequiredMixin):
         profiles = Profile.objects.filter(id=self.request.user.id).first()
 
         return profiles
+
+class ProfileEditView(LoginRequiredMixin, generic.UpdateView):
+    """社員詳細画面編集"""
+    model = Profile
+    # slug_url_kwarg = "id"
+    # slug_field =
+    template_name = 'ENP002_employee.html'
+    form_class = ProfileEditForm
+    success_url = reverse_lazy('profile_app:profile')
+
+    def get_form_kwargs(self, *args, **kwargs):
+        form_kwargs = super().get_form_kwargs(*args, **kwargs)
+
+        my_data = get_object_or_404(Profile, user=self.request.user)
+        edit_data = self.kwargs['pk']
+        logger.info("\n編集のユーザー：{}\nログインユーザー：{}".format(edit_data, my_data.id))
+        if my_data.id != edit_data:
+            logger.error("自分のプロフィールではない！！！")
+            raise PermissionDenied
+
+        # form_kwargs['initial'] = {'test2': my_data.species}
+        return form_kwargs
+
+    def form_valid(self, form):
+        # 元々のソース
+        form = form_save(self.request, form, 'プロフィール更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新が失敗しました。")
+        return super().form_invalid(form)
